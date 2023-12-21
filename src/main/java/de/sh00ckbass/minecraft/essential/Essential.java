8c82b4bf516d62e9d3a6235085512327e9ae7670
Tobias Zechmann <zechmann.tobias@t-online.de>
2023-12-21 19:29:40 +0100

    create basic command and listener logic (#1)

--- src/main/java/de/sh00ckbass/minecraft/essential/Essential.java
+++ src/main/java/de/sh00ckbass/minecraft/essential/Essential.java
@@ -1,17 +1,23 @@
 package de.sh00ckbass.minecraft.essential;
 
+import de.sh00ckbass.minecraft.essential.commands.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Essential extends JavaPlugin {
 
+    private final CommandManager commandManager;
+
+    public Essential(CommandManager commandManager) {
+        this.commandManager = commandManager;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.commandManager.setupCommands();
+        this.commandManager.setupListener();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
