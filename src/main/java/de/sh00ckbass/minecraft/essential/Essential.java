3d923bf5e3711b07acb1543091847706edb4c18a
Tobias Zechmann <zechmann.tobias@t-online.de>
2023-12-21 17:14:46 +0100

    init

---
+++ src/main/java/de/sh00ckbass/minecraft/essential/Essential.java
@@ -0,0 +1,17 @@
+package de.sh00ckbass.minecraft.essential;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Essential extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
